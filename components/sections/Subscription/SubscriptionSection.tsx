06643f08b745cb398a84401c56ef0152de138cc1
hunt-tickets <info@hunt-tickets>
2026-02-17 00:00:03 -0500

    fix: Move product images higher in subscription box

--- components/sections/Subscription/SubscriptionSection.tsx
+++ components/sections/Subscription/SubscriptionSection.tsx
@@ -68,7 +68,7 @@ export default function SubscriptionSection() {
               >
                 <motion.div
                   style={{ y: imageY }}
-                  className="relative w-full h-[120%] -top-[10%]"
+                  className="relative w-full h-[120%] -top-[25%]"
                 >
                   <Image
                     src={v.src}
