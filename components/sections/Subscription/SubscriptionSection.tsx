7b95d62db91c136a7136ea51f059fd01904a2ca2
hunt-tickets <info@hunt-tickets>
2026-02-17 00:24:42 -0500

    revert: Restore original image size and position

--- components/sections/Subscription/SubscriptionSection.tsx
+++ components/sections/Subscription/SubscriptionSection.tsx
@@ -67,7 +67,7 @@ export default function SubscriptionSection() {
               >
                 <motion.div
                   style={{ y: imageY }}
-                  className="absolute inset-x-0 h-[260px] sm:h-[300px] md:h-[340px] -top-[60px]"
+                  className="absolute inset-x-0 h-[360px] sm:h-[420px] md:h-[480px] -top-[80px]"
                 >
                   <Image
                     src={v.src}
